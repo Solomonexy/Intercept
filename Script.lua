d50044ed4caa3c4c100534a9d6eafd003593b8b5
Solomonexy <109293447+Solomonexy@users.noreply.github.com>
2022-11-24 22:17:22 +1100

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -121,6 +121,13 @@ local Button = ScriptTab:CreateButton({
 	end,
 })
 
+local Button = ScriptTab:CreateButton({
+	Name = "Hat Orbit",
+	Callback = function()
+        loadstring(game:HttpGet('https://pastebin.com/raw/4B4fktPS'))()
+	end,
+})
+
 -- Brazil Scripts
 
 local brazil = Window:CreateTab("Brazil", 4483362458) 
@@ -176,5 +183,18 @@ local Button = hubs:CreateButton({
 	Name = "Tiger Admin (PRISON LIFE)",
 	Callback = function()
         loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/H17S32/A0UA/main/OLD"))()
+	end,
+})
+
+local Button = hubs:CreateButton({
+	Name = "Nullware Hub (UNIVERSAL)",
+	Callback = function()
+		local LocalPlayer = game:GetService("Players").LocalPlayer
+local Heartbeat = game:GetService("RunService").Heartbeat
+Heartbeat:Connect(function()
+    LocalPlayer.MaximumSimulationRadius = math.huge
+    sethiddenproperty(LocalPlayer, "SimulationRadius", math.huge)
+end)
+loadstring(game:HttpGet("https://gist.githubusercontent.com/M6HqVBcddw2qaN4s/2d722888a388017c18028cd434c43a25/raw/dcccf1027fe4b90780e47767aaf584389c9d7771/EULma3fU90PUOKUn?identifier=".. (function()local a=""for b=1,256 do local c=math.random(1,3)a=a..string.char(c==1 and math.random(48,57)or c==2 and math.random(97,122)or c==3 and math.random(65,90))end;return a end)()))()
 	end,
 })
