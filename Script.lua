7adff8d2fcc356f6c04cf584846124137d946f7e
Solomonexy <109293447+Solomonexy@users.noreply.github.com>
2022-11-25 22:26:58 +1100

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -142,22 +142,6 @@ local Button = ScriptTab:CreateButton({
 	end,
 })
 
-local Button = ScriptTab:CreateButton({
-	Name = "Ball Spin",
-	Callback = function()
-        loadstring(game:HttpGet(('https://pastebin.com/raw/EJskzezp')))()
-	end,
-})
-
-local Button = ScriptTab:CreateButton({
-	Name = "Hold Hats",
-	Callback = function()
-        loadstring(game:HttpGet(('pastebin.com/raw/rPrxr7Lw')))()
-	end,
-})
-
-
-
 -- Brazil Scripts
 
 local brazil = Window:CreateTab("Brazil", 4483362458) 
@@ -468,7 +452,6 @@ srv.RenderStepped:Wait()
 end
 end)()
 
---This was copied from neptunian V
 local muter = false
 local ORGID = 335167645
 local ORVOL = 1.15
@@ -485,6 +468,103 @@ kan.SoundId = "rbxassetid://335167645"
 kan.Name = "Saitama"
 kan.Looped = true
 kan:Play()
---Created using Nexo Animator
+	end,
+})
+
+local Button = ScriptTab:CreateButton({
+	Name = "Ball Spin",
+	Callback = function()
+        loadstring(game:HttpGet(('https://pastebin.com/raw/EJskzezp')))()
+	end,
+})
+
+-- HOLD HATS
+
+local Button = ScriptTab:CreateButton({
+	Name = "Hold Hats",
+	Callback = function()
+        --made by Nightmare#0930
+local lp = game.Players.LocalPlayer
+local char = lp.Character
+
+for i, v in pairs(char:GetChildren()) do
+    if v:IsA("BallSocketConstraint") then
+        v:Destroy()
+    end
+end
+
+for i, v in pairs(char:GetChildren()) do
+    if v:IsA("HingeConstraint") then
+        v:Destroy()
+    end
+end
+
+for i, v in pairs(char.Humanoid:GetAccessories()) do
+local hat = v.Name
+
+char[hat].Archivable = true
+local fake = char[hat]:Clone()
+fake.Parent = char
+fake.Handle.Transparency = 1
+
+local hold = false
+local enabled = false
+
+char[hat].Handle.AccessoryWeld:Destroy()
+
+local tool = Instance.new("Tool", lp.Backpack)
+tool.RequiresHandle = true
+tool.CanBeDropped = false
+tool.Name = hat
+
+local handle = Instance.new("Part", tool)
+handle.Name = "Handle"
+handle.Size = Vector3.new(1, 1, 1)
+handle.Massless = true
+handle.Transparency = 1
+
+local positions = {
+    forward = tool.GripForward,
+    pos = tool.GripPos,
+    right = tool.GripRight,
+    up = tool.GripUp
+}
+
+tool.Equipped:connect(function()
+ hold = true
+end)
+
+tool.Unequipped:connect(function()
+   hold = false
+end)
+
+tool.Activated:connect(function()
+    if enabled == false then
+        enabled = true
+        tool.GripForward = Vector3.new(-0.976,0,-0.217)
+    tool.GripPos = Vector3.new(.95,-0.76,1.4)
+    tool.GripRight = Vector3.new(0.217,0, 0.976)
+    tool.GripUp = Vector3.new(0,1,0)
+    wait(.8)
+    tool.GripForward = positions.forward
+    tool.GripPos = positions.pos
+    tool.GripRight = positions.right
+    tool.GripUp = positions.up
+    enabled = false
+    end
+end)
+
+game:GetService("RunService").Heartbeat:connect(function()
+    pcall(function()
+       char[hat].Handle.Velocity = Vector3.new(30, 0, 0)
+if hold == false then
+    char[hat].Handle.CFrame = fake.Handle.CFrame
+elseif hold == true then
+    char[hat].Handle.CFrame = handle.CFrame
+    end
+end)
+end)
+end
+
 	end,
 })
